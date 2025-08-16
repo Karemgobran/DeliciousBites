4708461b0aa6de0015f4e69e31f249683ea829df
Karem Gobran <112480829+Karemgobran@users.noreply.github.com>
2025-08-16 18:42:58 +0300

    feat: Add OurStory component with styling and integrate into Home page style: Update Header styles for sticky positioning and adjust HeroSection padding

---
+++ src/components/OurStory/OurStory.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./OurStory.scss";
+function OurStory() {
+  return (
+    <section className="our-story d-flex justify-content-between align-items-center">
+      <div className="our-story_content d-flex flex-column">
+        <h2>
+          <span>Our</span> Story
+        </h2>
+        <p>
+          At Delicious Bites, we believe in the power of good food to bring
+          people together. Our journey began with a simple passion for creating
+          products that delight the senses and nourish the body.
+        </p>
+        <p>
+          Today, we continue to source the finest ingredients from trusted
+          suppliers, crafting food and beverages that honor traditional
+          techniques while embracing innovative flavors.
+        </p>
+      </div>
+      <div className="our-story_image">
+        <img
+          src="https://images.unsplash.com/photo-1556910103-1c02745aae4d"
+          alt="OurStory image"
+        />
+      </div>
+    </section>
+  );
+}
+
+export default OurStory;
--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./Home.scss";
 import HeroSection from "../../components/HeroSection/HeroSection";
 import FeaturedProducts from "../../components/FeaturedProducts/FeaturedProducts";
+import OurStory from "../../components/OurStory/OurStory";
 function Home() {
   return (
     <div>
       <HeroSection />
       <FeaturedProducts />
+      <OurStory />
     </div>
   );
 }
